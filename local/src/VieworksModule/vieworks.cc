4e9e9d06cd0a223ffd1cc918511b0237182cc840
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2019-02-19 17:03:15 -0600

    changed new instance in vieworks

--- local/src/VieworksModule/vieworks.cc
+++ local/src/VieworksModule/vieworks.cc
@@ -129,7 +129,7 @@ NAN_METHOD(vwCam::New) {
     const int argc = 1;
     v8::Local<v8::Value> argv[argc] = { info[0] };
     v8::Local<v8::Function> cons = Nan::New<v8::Function>(constructor);
-    info.GetReturnValue().Set(cons->NewInstance(argc, argv));
+    info.GetReturnValue().Set(Nan::NewInstance(cons,argc, argv));
   }
 }
 
