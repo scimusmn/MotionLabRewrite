e8b0ff96502fb102f97051a14280898a9a37f596
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2019-02-19 16:05:21 -0600

    handle debug output

--- aux_install.sh
+++ aux_install.sh
@@ -4,10 +4,10 @@ AUX_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd $AUX_DIR
 
-sudo apt install libfreeimage-dev 2> $OUTPUT
+sudo apt install libfreeimage-dev >$1
 
-sudo apt install node-gyp 2>$OUTPUT
+sudo apt install node-gyp >$1
 
-bash $AUX_DIR/local/src/VieworksModule/VIS-Shadow-Install/install.sh
+bash $AUX_DIR/local/src/VieworksModule/VIS-Shadow-Install/install.sh $1
 
 sudo chmod -R 777 /usr/include/VIS-Shadow
--- local/src/VieworksModule/VIS-Shadow-Install/install.sh
+++ local/src/VieworksModule/VIS-Shadow-Install/install.sh
@@ -2,6 +2,10 @@
 
 set -o errexit
 
+CMD_OUTPUT=$1
+
+echo "Output is going to $CMD_OUTPUT"
+
 VIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 
@@ -14,17 +18,17 @@ echo ""
 echo "    Please wating for a few minutes while downloading and installing  "
 echo ""
 echo "   install 1 of 7 .... zlib1g-dev "
-sudo apt-get install zlib1g-dev -y >${OUTPUT} 2>&1
+sudo apt-get install zlib1g-dev -y >${CMD_OUTPUT} 2>&1
 echo "   install 2 of 7 .... gcc "
-sudo apt-get install gcc -y >${OUTPUT} 2>&1
+sudo apt-get install gcc -y >${CMD_OUTPUT} 2>&1
 echo "   install 3 of 7 .... g++ "
-sudo apt-get install g++ -y >${OUTPUT} 2>&1
+sudo apt-get install g++ -y >${CMD_OUTPUT} 2>&1
 echo "   install 4 of 7 .... libqtcore4 "
-sudo apt-get install libqtcore4 -y >${OUTPUT} 2>&1
+sudo apt-get install libqtcore4 -y >${CMD_OUTPUT} 2>&1
 echo "   install 6 of 7 .... libqt4-dev "
-sudo apt-get install libqt4-dev -y >${OUTPUT} 2>&1
+sudo apt-get install libqt4-dev -y >${CMD_OUTPUT} 2>&1
 echo "   install 7 of 7 .... libqtgui4 "
-sudo apt-get install libqtgui4 -y >${OUTPUT} 2>&1
+sudo apt-get install libqtgui4 -y >${CMD_OUTPUT} 2>&1
 
 echo ""
 echo "    Other Package Installation's Completion!"
@@ -32,7 +36,7 @@ echo ""
 
 echo "Installing VIS-Shadow packages...";
 
-sudo dpkg -i $VIS_DIR/VIS-Shadow_1.1.0_x86_64.deb >${OUTPUT} 2>&1
+sudo dpkg -i $VIS_DIR/VIS-Shadow_1.1.0_x86_64.deb >${CMD_OUTPUT} 2>&1
 
 #sudo sh genicam.sh
 #sudo sh netsetting.sh
