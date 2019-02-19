16047b5676f5be953abccc2e095338c385997167
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2019-02-19 16:07:42 -0600

    handle debug output

--- aux_install.sh
+++ aux_install.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+CMD_OUTPUT=$1
+
+echo "Output is going to $CMD_OUTPUT"
+
 AUX_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd $AUX_DIR
 
-sudo apt install libfreeimage-dev >$1
+sudo apt-get install libfreeimage-dev >$1 2>&1
 
-sudo apt install node-gyp >$1
+sudo apt-get install node-gyp >$1 2>&1
 
 bash $AUX_DIR/local/src/VieworksModule/VIS-Shadow-Install/install.sh $1
 
