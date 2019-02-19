714a6e2e6b35491a16c736d73f95f4e455a0c6a3
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2019-02-19 13:43:28 -0600

    vieworks module library include changes

--- aux_install.sh
+++ aux_install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+AUX_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-cd $DIR
+cd $AUX_DIR
 
-bash $DIR/local/VieworksModule/VIS-Shadow-Install/install.sh
+
+
+bash $AUX_DIR/local/VieworksModule/VIS-Shadow-Install/install.sh
 
 sudo chmod -R 777 /usr/include/VIS-Shadow
--- local/VieworksModule/VIS-Shadow-Install/install.sh
+++ local/VieworksModule/VIS-Shadow-Install/install.sh
@@ -2,26 +2,7 @@
 
 set -o errexit
 
-PROMPT='# '
-YESNO_PROMPT='(y/n)# '
-
-# version of the software
-MAJOR_VERSION=1
-MINOR_VERSION=1
-# 0 Alpha, 1 Beta, 2 RC, 3 Public release
-RELEASE_TYPE=2
-RELEASE_BUILD=0
-
-echo "This is a script to assist with installation of the VIS-Shadow SDK.";
-echo "Would you like to continue and install all the VIS-Shadow SDK packages?";
-echo -n "$YESNO_PROMPT"
-read confirm
-
-if [ $confirm = "n" ] || [ $confirm = "N" ] || [ $confirm = "no" ] || [ $confirm = "No" ]
-then
-    exit 0
-    break
-fi
+VIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 
 echo "########################################################################"
@@ -38,10 +19,12 @@ echo "   install 2 of 7 .... gcc "
 sudo apt-get install gcc -y 2> error.log
 echo "   install 3 of 7 .... g++ "
 sudo apt-get install g++ -y 2> error.log
-echo "   install 4 of 7 .... libqt4-core "
-sudo apt-get install libqt4-designer libqt4-opengl libqt4-svg libqtgui4 libqtwebkit4 -y 2> error.log
+echo "   install 4 of 7 .... libqtcore4 "
+sudo apt-get install libqtcore4 -y 2> error.log
 echo "   install 6 of 7 .... libqt4-dev "
 sudo apt-get install libqt4-dev -y 2> error.log
+echo "   install 7 of 7 .... libqtgui4 "
+sudo apt-get install libqtgui4 -y 2> error.log
 
 echo ""
 echo "    Other Package Installation's Completion!"
@@ -49,24 +32,24 @@ echo ""
 
 echo "Installing VIS-Shadow packages...";
 
-sudo dpkg -i VIS-Shadow_1.1.0_x86_64.deb
+sudo dpkg -i $DIR/VIS-Shadow_1.1.0_x86_64.deb 2> error.log
 
 #sudo sh genicam.sh
 #sudo sh netsetting.sh
 
 echo "Complete";
-
-echo "Would you like to reboot the system?";
-echo -n "$YESNO_PROMPT"
-read confirm
-
-if [ $confirm = "n" ] || [ $confirm = "N" ] || [ $confirm = "no" ] || [ $confirm = "No" ]
-then
-    exit 0
-    break
-fi
-
-sudo reboot
-
+#
+# echo "Would you like to reboot the system?";
+# echo -n "$YESNO_PROMPT"
+# read confirm
+#
+# if [ $confirm = "n" ] || [ $confirm = "N" ] || [ $confirm = "no" ] || [ $confirm = "No" ]
+# then
+#     exit 0
+#     break
+# fi
+#
+# sudo reboot
+#
 
 exit 0
