0d1ccc95bec6ac6607e51b2518b2003c10b185aa
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2019-02-20 08:51:07 -0600

    only install vis once

--- aux_install.sh
+++ aux_install.sh
@@ -14,6 +14,10 @@ sudo apt-get -qq -o=Dpkg::Use-Pty=0 --assume-yes install node-gyp >$1 2>&1
 
 echo -e "\n* Install the Vieworks Libraries..."
 
-bash $AUX_DIR/local/src/VieworksModule/VIS-Shadow-Install/install.sh $CMD_OUTPUT
+if [[ -f "$AUX_DIR/../current/VIS_Installed" ]]; then
+  bash $AUX_DIR/local/src/VieworksModule/VIS-Shadow-Install/install.sh $CMD_OUTPUT
+fi
+
+touch $AUX_DIR/../current/VIS_Installed
 
 sudo chmod -R 777 /usr/include/VIS-Shadow
