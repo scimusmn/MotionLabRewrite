45e0602839fbc262bbef5988843ae43c31b564e4
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2019-02-20 11:53:51 -0600

    troubleshooting arduino

--- local/src/camera.js
+++ local/src/camera.js
@@ -3,7 +3,7 @@ var remote = require('electron').remote;
 
 var config = remote.getGlobal('config');
 
-//var vieworks = require('bindings')('vieworks');
+var vieworks = require('bindings')('vieworks');
 
 var requests = [
   './src/files.js',
@@ -16,44 +16,44 @@ if (!window.muse.app.camera) window.muse.app.camera = {
 var store = window.muse.app.camera;
 
 obtain(requests, (files)=> {
-  exports.init = ()=> {};//(cb)=>{
-  //   if(!store.cam){
-  //     store.cam = new vieworks.camera(()=> {
-  //       store.cam.setFrameRate(config.cam.frameRate);
-  //       store.cam.setImageGain(config.cam.imageGain);
-  //
-  //       store.cam.allocateBuffer(2 * config.cam.frameRate * config.record.time);
-  //
-  //       store.cam.startCapture(function (val) {
-  //         store.cam.ready = true;
-  //         //optionally draw the camera image. See appOld.js for details
-  //
-  //       });
-  //
-  //       if(cb) cb();
-  //     });
-  //   } else {
-  //     if(store.cam.ready) cb();
-  //   }
-  // }
+  exports.init = (cb)=> {
+    if (!store.cam) {
+      store.cam = new vieworks.camera(()=> {
+        store.cam.setFrameRate(config.cam.frameRate);
+        store.cam.setImageGain(config.cam.imageGain);
 
-  exports.isReady = ()=> {};///()=>store.cam.ready;
+        store.cam.allocateBuffer(2 * config.cam.frameRate * config.record.time);
 
-  exports.base = ()=> {};//store.cam;
+        store.cam.startCapture(function (val) {
+          store.cam.ready = true;
+          //optionally draw the camera image. See appOld.js for details
 
-  exports.capture = ()=> {};//store.cam.capture;
+        });
 
-  exports.endCapture = ()=> {};//(folderName, onSave)=>{
-  //   store.cam.stopCapture();
-  //
-  //   files.deleteFolder(folderName);//deleteFolderRecursive(dir);
-  //   files.makeFolder(folderName);
-  //   store.cam.save(folderName, function () {
-  //     files.touchFolder(folderName);
-  //     store.cam.ready = true;
-  //
-  //     cb();
-  //   });
-  // }
+        if (cb) cb();
+      });
+    } else {
+      if (store.cam.ready) cb();
+    }
+  };
+
+  exports.isReady = ()=>store.cam.ready;
+
+  exports.base = store.cam;
+
+  exports.capture = store.cam.capture;
+
+  exports.endCapture = (folderName, onSave)=> {
+    store.cam.stopCapture();
+
+    files.deleteFolder(folderName);//deleteFolderRecursive(dir);
+    files.makeFolder(folderName);
+    store.cam.save(folderName, function () {
+      files.touchFolder(folderName);
+      store.cam.ready = true;
+
+      cb();
+    });
+  };
 
 });
