b3794cea4410c3c70b03e4a90fc9f2ef38be3f22
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2019-02-20 07:58:36 -0600

    correcting code errors

--- local/src/camera.js
+++ local/src/camera.js
@@ -17,44 +17,44 @@ if(!window.muse.app.camera ) window.muse.app.camera = {
 var store = window.muse.app.camera;
 
 obtain(requests, (files)=> {
-  exports.init = (cb)=>{
-    if(!store.cam){
-      store.cam = new vieworks.camera(()=> {
-        store.cam.setFrameRate(config.cam.frameRate);
-        store.cam.setImageGain(config.cam.imageGain);
+  exports.init = ()=>{};//(cb)=>{
+  //   if(!store.cam){
+  //     store.cam = new vieworks.camera(()=> {
+  //       store.cam.setFrameRate(config.cam.frameRate);
+  //       store.cam.setImageGain(config.cam.imageGain);
+  //
+  //       store.cam.allocateBuffer(2 * config.cam.frameRate * config.record.time);
+  //
+  //       store.cam.startCapture(function (val) {
+  //         store.cam.ready = true;
+  //         //optionally draw the camera image. See appOld.js for details
+  //
+  //       });
+  //
+  //       if(cb) cb();
+  //     });
+  //   } else {
+  //     if(store.cam.ready) cb();
+  //   }
+  // }
 
-        store.cam.allocateBuffer(2 * config.cam.frameRate * config.record.time);
+  exports.isReady = ()=>{};///()=>store.cam.ready;
 
-        store.cam.startCapture(function (val) {
-          store.cam.ready = true;
-          //optionally draw the camera image. See appOld.js for details
+  exports.base = ()=>{};//store.cam;
 
-        });
+  exports.capture = ()=>{};//store.cam.capture;
 
-        if(cb) cb();
-      });
-    } else {
-      if(store.cam.ready) cb();
-    }
-  }
-
-  exports.isReady = ()=>store.cam.ready;
-
-  exports.base = store.cam;
-
-  exports.capture = store.cam.capture;
-
-  exports.endCapture = (folderName, onSave)=>{
-    store.cam.stopCapture();
-
-    files.deleteFolder(folderName);//deleteFolderRecursive(dir);
-    files.makeFolder(folderName);
-    store.cam.save(folderName, function () {
-      files.touchFolder(folderName);
-      store.cam.ready = true;
-
-      cb();
-    });
-  }
+  exports.endCapture = ()=>{};//(folderName, onSave)=>{
+  //   store.cam.stopCapture();
+  //
+  //   files.deleteFolder(folderName);//deleteFolderRecursive(dir);
+  //   files.makeFolder(folderName);
+  //   store.cam.save(folderName, function () {
+  //     files.touchFolder(folderName);
+  //     store.cam.ready = true;
+  //
+  //     cb();
+  //   });
+  // }
 
 }
