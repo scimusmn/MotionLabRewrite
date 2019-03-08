a04d2c5fc54a029f77f9769171b909abd30d7169
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2019-03-08 14:04:46 -0600

    figuring out flags

--- config/ifaceConfig.js
+++ config/ifaceConfig.js
@@ -5,10 +5,12 @@ global.obtain = (addr, func)=> {
 
 obtain(['fs', `${__dirname}/../../configurator/src/utils.js`], (fs, utils)=> {
 
+  var args = (key)=>(~process.argv.indexOf(key)) ? process.argv[process.argv.indexOf(key) + 1] : false;
+
   var opts = process.argv;
 
-  var cam = (~opts.indexOf('--cam')) ? opts[opts.indexOf('--cam')] : 'eth0';
-  var net = (~opts.indexOf('--net')) ? opts[opts.indexOf('--net')] : 'eth1';
+  var cam = (args('--cam')) ? args('--cam') : 'eth0';
+  var net = (args('--net')) ? args('--net') : 'eth1';
 
   console.log('setting netplan config...');
 
